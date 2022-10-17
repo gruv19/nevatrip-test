8e4d9ed2ad1f2ee17cc2de5dd4797ae35d66b8b9
gruv19 <grv1992@yandex.ru>
2022-10-17 21:43:01 +0500

    feat: реализовал компонент time-forward-select

---
+++ 2_2_time/components/time-forward-select/index.js
@@ -0,0 +1,31 @@
+import select from '../select';
+import './time-forward-select.scss';
+
+import { prepareTime } from '../../utils';
+
+const schedule = [ '18:00', '18:30', '18:45', '19:00', '19:15', '21:00' ];
+
+function generateOptions(schedule) {
+  return schedule.map(time => {
+    return { value: `${time}(из A в B)`, text: `${prepareTime(time)}(из A в B)` }
+  });
+}
+
+function timeForwardSelect() {
+  const selectData = { 
+    name: 'time-forward', 
+    label: 'Выбери время выезда из A в B', 
+    options: generateOptions(schedule),
+    // options: [ 
+    //   { value: '18:00(из A в B)', text: '18:00(из A в B)' },  
+    //   { value: '18:30(из A в B)', text: '18:30(из A в B)' },  
+    //   { value: '18:45(из A в B)', text: '18:45(из A в B)' },  
+    //   { value: '19:00(из A в B)', text: '19:00(из A в B)' },  
+    //   { value: '19:15(из A в B)', text: '19:15(из A в B)' },  
+    //   { value: '21:00(из A в B)', text: '21:00(из A в B)' },  
+    // ],
+  };
+  return select(selectData);
+}
+
+export default timeForwardSelect;
